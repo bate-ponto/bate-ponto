ae87c1b62f6c093d8f175993c01a9f3641741ae5
bate-ponto <103081934+bate-ponto@users.noreply.github.com>
2022-04-19 21:36:24 -0300

    Change a lot of things

--- src/components/TimeRegistered.js
+++ src/components/TimeRegistered.js
@@ -1,18 +1,24 @@
-import React from 'react'
+import React from "react";
 
 export default function TimeRegistered({
-    beginTime = '08:00',
-    endTime = '12:00',
-    duration = '04:00'
+  beginTime = "08:00",
+  endTime = "12:00",
+  duration = "04:00"
 }) {
-    return (
-        <div className="time-container">
-            <input className="description" type="text"></input>
-            <div className='information'>
-                <span className='begin-time' name="beginTime">{beginTime}</span>
-                <span className='end-time' name="endTime">{endTime}</span>
-                <span className='duration' name="duration">{duration}</span>
-            </div>
-        </div>
-    )
+  return (
+    <div className="time-container">
+      <input className="description" type="text" />
+      <div className="information">
+        <span className="begin-time" name="beginTime">
+          {beginTime}
+        </span>
+        <span className="end-time" name="endTime">
+          {endTime}
+        </span>
+        <span className="duration" name="duration">
+          {duration}
+        </span>
+      </div>
+    </div>
+  );
 }
