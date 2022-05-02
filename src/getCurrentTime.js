df692d72f828214b0d5b9d1d01ef1bf314e24d38
Gabriel Ramos <gabrielramos791@gmail.com>
2022-05-02 20:15:36 -0300

    convert time to HH:mm:ss

--- src/getCurrentTime.js
+++ src/getCurrentTime.js
@@ -1,4 +1,4 @@
 export default function getCurrentTime() {
-  const now = new Date();
-  return now.getMinutes() * 60 + now.getSeconds();
+	const now = new Date();
+	return (now.getHours() * 3600) + (now.getMinutes() * 60) + now.getSeconds();
 }
--- src/transformAsTime.js
+++ src/transformAsTime.js
@@ -1,10 +1,13 @@
 export default function transformAsTime(time) {
-  const minutes = Math.floor(time / 60);
-  const seconds = time % 60;
+	const hours = Math.floor(time / 3600);
+	const minutes = Math.floor((time - (hours * 3600)) / 60);
+	const seconds = time - (hours * 3600) - (minutes * 60);
 
-  return (
-    minutes.toString().padStart(2, "0") +
-    ":" +
-    seconds.toString().padStart(2, "0")
-  );
+	return (
+		hours.toString().padStart(2, '0') +
+		':' +
+		minutes.toString().padStart(2, '0') +
+		':' +
+		seconds.toString().padStart(2, '0')
+	);
 }
